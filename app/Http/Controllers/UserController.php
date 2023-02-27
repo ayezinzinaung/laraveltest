a3b1c732c829cb8e6658f9022ba21eaa036c1069
ayezinzinaung <ayezin2aung101120@gmail.com>
2023-02-27 13:28:22 +0630

    first update

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(){
+        return 'This is show function from UserController';
+    }
+}
